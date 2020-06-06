27cbf10401f5c3690e09a5e0c428c88e364ae0b2
kinshuk <kinshuk2jain@gmail.com>
2020-06-06 15:27:05 +0200

    Improve documentation

--- src/constants/timers.js
+++ src/constants/timers.js
@@ -1,11 +1,11 @@
-// 60 sec
+// 60 sec (expressed in ms)
 const REQUEST_TIMEOUT = 60 * 1000
 
-// 15 min
+// 15 min (expressed in s)
 const JWT_EXPIRY_TIMEOUT = 15 * 60 * 60
 
-// 8 hours
-const REFRESH_TOKEN_EXPIRY_TIMEOUT = 8 * 60 * 60 * 1000
+// 30 days (expressed in ms)
+const REFRESH_TOKEN_EXPIRY_TIMEOUT = 30 * 24 * 60 * 60 * 1000
 
 module.exports = {
   REFRESH_TOKEN_EXPIRY_TIMEOUT,
